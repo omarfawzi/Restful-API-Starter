6bfd06de9f38d72accd44cc8d86cef3cd7d8608b
omar.fawzy <omar.fawzy@aboutyou.com>
2022-07-01 15:39:27 +0200

    protect file

--- app/Modules/OpenApi/Contexts/OpenApiContext.php
+++ app/Modules/OpenApi/Contexts/OpenApiContext.php
@@ -5,7 +5,7 @@ namespace App\Modules\OpenApi\Contexts;
 use cebe\openapi\spec\OpenApi;
 use League\OpenAPIValidation\PSR7\OperationAddress;
 
-class OpenApiContext
+final class OpenApiContext
 {
     public function __construct(public OpenApi $openApi, public OperationAddress $operationAddress){}
 }
