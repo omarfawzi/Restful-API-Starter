170c475c2cdda68458bbc048c3793140c6c23f1f
omar.fawzy <omar.fawzy@aboutyou.com>
2022-07-04 17:00:07 +0200

    add no conditions trait

---
+++ app/Modules/Api/Conditions/NoConditionsTrait.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\Api\Conditions;
+
+trait NoConditionsTrait
+{
+    public function getConditions(): array
+    {
+        return [];
+    }
+}
--- app/Modules/User/Requests/GetUsers.php
+++ app/Modules/User/Requests/GetUsers.php
@@ -2,6 +2,7 @@
 
 namespace App\Modules\User\Requests;
 
+use App\Modules\Api\Conditions\NoConditionsTrait;
 use App\Modules\Api\Handlers\ApiRequestHandler;
 use App\Modules\Api\Responses\ApiResponse;
 use App\Modules\Api\Utilities\ApiFilter;
@@ -14,15 +15,12 @@ use Nyholm\Psr7\Response;
 
 class GetUsers extends ApiRequestHandler
 {
+    use NoConditionsTrait;
+
     public function __construct(
         private UserTransformer $transformer,
         private UserService $service
     ) {}
-
-    public function getConditions(): array
-    {
-        return [];
-    }
 
     public function processRequest(Request $request): Response
     {
